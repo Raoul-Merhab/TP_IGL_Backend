6661aedffb5aec909126c0c4894e595dcbd715e3
farahyahi <lf_yahi@esi.dz>
2024-01-06 00:00:13 +0100

    validators added

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from routes.Authentification import router as auth_router
 from routes.Administrateur import router as admin_router
+from routes.Article_Favori import router as article_router
 
 router = APIRouter()
 
 router.include_router(router=auth_router, prefix="/auth", tags=["Authentification"])
 router.include_router(router=admin_router, prefix="/admin", tags=["Administrateur"])
+router.include_router(router=article_router,prefix="/article", tags=["Article"])
